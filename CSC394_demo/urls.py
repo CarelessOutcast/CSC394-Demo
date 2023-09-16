3c1f09cd6941b0df4de929c948b86d4d98f7374a
Austin Q. Truong <auqtruo@gmail.com>
2023-09-16 11:50:48 -0500

    change views and urls file; added templates for functions

--- CSC394_demo/urls.py
+++ CSC394_demo/urls.py
@@ -1,7 +1,9 @@
-
 from django.urls import path
-from . import views
+from .views import task_list, add_task, update_task, delete_task
 
 urlpatterns = [
-        path("", views.index, name="index"),
-        ]
+        path('', task_list.as_view(), name='redlobsterTM'),
+        path('task/add', add_task.as_view(), name='task_add'),
+        path('task/<int:pk>/update/', update_task.as_view(), name='task_update'),
+        path('task/<int:pk>/delete/', delete_task.as_view(), name='task_delete')
+]
--- CSC394_demo/views.py
+++ CSC394_demo/views.py
@@ -1,21 +1,35 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.views.generic import ListView
+from django.views.generic.edit import UpdateView, CreateView, DeleteView 
 from .models import task_model
+
 
 # Create your views here.
 
 def index(request):
     return render(request,'redlobsterHome.html')
 
+#list
+def task_list(list_view):
+    model           = task_model
+    template_name   = 'redlobsterHome.html'
+    
 #insert
-def add_task(request):
-    task_title = request.POST['task_title']
-    task_model.objects.create(task_title=task_title)
-    return 
+def add_task(CreateView):
+    model           = task_model
+    template_name   = 'task_add.html'
+    fields          = '__all__'
+    success_url     = redirect('redlobsterTM')
     
 #update
-def update_task(request):
-    pass
-
+def update_task(UpdateView):
+    model           = task_model
+    template_name   = 'task_update.html'
+    fields          = '__all__'
+    success_url     = redirect('redlobsterTM')
+    
 #delete
-def delete_task(request):
-    pass
+def delete_task(DeleteView):
+    model           = task_model
+    template_name   = 'task_delete.html'
+    success_url     = redirect('redlobsterTM')
