e80bdfe345a1f7d90a181a74f6ceac0230d43bcc
Austin Q. Truong <99311122+Auqtruong@users.noreply.github.com>
2023-09-16 22:42:53 -0500

    Revert "changes to views and models"

--- CSC394_demo/urls.py
+++ CSC394_demo/urls.py
@@ -8,7 +8,5 @@ urlpatterns = [
         path("taskmanager", views.taskmanager, name="taskmanager"),
         path("login", views.login_view, name="login"),
         path("logout", views.logout_view, name="logout"),
-        path('task/add', views.add_task, name='task_add'),
-        path('task/<int:pk>/update', views.update_task , name='task_update'),
-        path('task/<int:pk>/delete/', views.delete_task, name='task_delete')
-]
+        path("add_task/<str:type>", views.add_task, name="add_task"),
+        ]
